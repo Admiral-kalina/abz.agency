13ddff68bc484b7a6384f930ac04e52ca34e96e6
Admiral-kalina <kalinamono@gmail.com>
2022-05-30 16:04:12 +0200

    first com

---
+++ src/Ui/Button/YellowBtn.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const YellowBtn = (props) => {
+    console.log(props)
+    return (
+        <Button {...props}>{props.children}</Button>
+    );
+};
+
+const Button = styled.button`
+  font-size: 16px;
+  line-height: 26px;
+  background-color: #f4E041;
+  width: 100px;
+  height: 34px;
+  border: none;
+  border-radius: 15px;
+`
+
+export default YellowBtn;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import styled from 'styled-components';
+import YellowBtn from "../../Ui/Button/YellowBtn";
+import Logo from "./img/logo.svg"
+
+const Main = () => {
+    return (
+        <MainContainer>
+            <MainHeader>
+                <MainLogo>
+                    <Image src={Logo}/>
+                    <LogoText>Testtask</LogoText>
+                </MainLogo>
+                <MainBtns>
+                    <YellowBtn a={'asd'}>Users</YellowBtn>
+                    <YellowBtn a={'asd'}>Sing up</YellowBtn>
+                </MainBtns>
+            </MainHeader>
+            <MainBlock>
+                <MainContant>
+                    <ContentHeading>Test assignment for front-end developer</ContentHeading>
+                    <ContentText>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</ContentText>
+                    <YellowBtn>Sing up</YellowBtn>
+                </MainContant>
+
+            </MainBlock>
+        </MainContainer>
+    );
+};
+
+export default Main;
+
+const MainContainer = styled.div`
+  max-width: 1190px;
+  margin: 10px auto;
+`
+const MainHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+const MainLogo = styled.div`
+  display: flex;       
+`
+
+const Image = styled.img`
+`
+const LogoText = styled.p`
+  text-transform: uppercase;
+`
+
+const MainBtns = styled.div`
+display: flex;
+`
+
+
+
+
+const MainBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  background-image: url('https://abzagenci.imgix.net/abzagency/main/bg/bg.png');
+  width: 100%;
+  height: 650px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-color: black;
+  position: relative;
+  :after{
+    content:'';
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color:rgba(0,0,0,.5);
+  }
+`
+const MainContant = styled.div`
+  color: azure;
+  position: absolute;
+  height: 50%;
+  z-index: 5;
+  display: flex;
+  flex-wrap: wrap;
+  max-width:  380px;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+`
+const ContentHeading = styled.h1`
+    text-align: center;
+   
+`
+const ContentText = styled.p`
+`
+
+
